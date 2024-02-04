324973cdd57dc8081f76ead21aad4456f2458dea
Michael Eckebrecht <direkt@route-media.info>
2024-02-04 23:27:07 +0100

    Update UpgradeSchema.php

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -10,51 +10,42 @@ class UpgradeSchema implements UpgradeSchemaInterface
 {
     public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
-        if (version_compare($context->getVersion(), '1.0.2', '<')) {
-            $installer = $setup;
-            $installer->startSetup();
-            /**
-             * update column 'overview'
-            **/
 
+        if (version_compare($context->getVersion(), '1.0.2', '<')) {
+            // Füge das neue Feld hinzu
             $table = $installer->getTable('routemedia_rfq');
 
-            $installer->getConnection()->modifyColumn(
+            $installer->getConnection()->addColumn(
                 $table,
                 'requestData',
                 [
-                        'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-                        'length' => '64k',
-                        'nullable' => true,
-                        'default' => null,
-                        'comment' => 'Request Data'
+                    'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    'length' => '64k',
+                    'nullable' => true,
+                    'comment' => 'Request Data',
                 ]
             );
-            $installer->endSetup();
         }
 
         else if (version_compare($context->getVersion(), '1.0.1', '<')) {
             $installer = $setup;
             $installer->startSetup();
-                    /**
-                     * update column 'overview'
-                    **/
-
-                    $table = $installer->getTable('routemedia_rfq');
-
-                    $installer->getConnection()->modifyColumn(
-                        $table,
-                        'overview',
-                        [
-                                'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-                                'length' => '64k',
-                                'nullable' => true,
-                                'default' => null,
-                                'comment' => 'Overview'
-                        ]
-                    );
-
-
+            /**
+             * update column 'overview'
+            **/
+            $table = $installer->getTable('routemedia_rfq');
+            
+            $installer->getConnection()->modifyColumn(
+                $table,
+                'overview',
+                [
+                        'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                        'length' => '64k',
+                        'nullable' => true,
+                        'default' => null,
+                        'comment' => 'Overview'
+                ]
+            );
             $installer->endSetup();
         }
     }
